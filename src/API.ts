de25c131f702831456575dc80b22ebbe70de14d5
Marco Antonio Fuentes Jimenez <markfuentes2991@gmail.com>
2021-03-28 16:43:16 -0600

    New Api key for dummy rest

--- src/API.ts
+++ src/API.ts
@@ -1,7 +1,7 @@
 import {userPost} from './tReducer';
 export const getPosts = async (limit:userPost["limit"]) => {
     const posts = await (
-      await fetch(`https://dummyapi.io/data/api/post?limit=${limit}`, { headers: { 'app-id': '60602dd031e8510f243fb15b' } })
+      await fetch(`https://dummyapi.io/data/api/post?limit=${limit}`, { headers: { 'app-id': '6061066405a27f903f3f0e1c' } })
     ).json();
     return posts.data;
   };
